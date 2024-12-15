a6267afe96934d9c9e64958294295c78a5f5b19c
sohilametwaly <sohailarafat934@yahoo.com>
2024-12-15 19:56:37 +0200

    firebase linking

---
+++ lib/Database.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'firebase_options.dart';
+
+class Database {
+  static final FirebaseDatabase _database = FirebaseDatabase.instance;;
+}
+
+// await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
