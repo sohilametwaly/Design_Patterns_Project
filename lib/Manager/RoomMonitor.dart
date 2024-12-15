95e80d0e8ecf753659fc115bdcfa553da6b7ed2f
BasharMOhamed <bashar.eg6645@gmail.com>
2024-12-15 22:29:17 +0200

    Rebasing with the singleton branch

--- lib/Manager/RoomMonitor.dart
+++ lib/Manager/RoomMonitor.dart
@@ -1,8 +1,10 @@
+import 'package:design_patterns_project/Database.dart';
 import 'package:design_patterns_project/Manager/RoomMonitoring.dart';
 
 class Roommonitor implements Roommonitoring {
+  final _db = Database.getInstance();
   @override
-  Map<String, Map<String, dynamic>> monitorRooms() {
-    return Database.getInstance().read("rooms");
+  Future<Map<String, Map<String, dynamic>>> monitorRooms() async {
+    return await _db.readData("rooms");
   }
 }
