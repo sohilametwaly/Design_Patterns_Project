3220d09ba53cad89877709cba2a2087a1447da4d
engy medhat <mengy289@gmail.com>
2024-12-15 20:50:32 +0200

    Authentication and worker class

---
+++ lib/realauth.dart
@@ -0,0 +1,18 @@
+import 'package:design_patterns_project/authentication.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class realauth implements Authentication {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<void> login(String email, String password) async {
+    await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<void> forgetpassword(String email) async {
+    await auth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> signup(String username, String password, String email) async {
+    await auth.createUserWithEmailAndPassword(email: email, password: password);
+  }
+}
