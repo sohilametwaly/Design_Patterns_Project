42b9537b528239b3cc83241ded157dbb042ef9c4
engy medhat <mengy289@gmail.com>
2024-12-19 15:23:02 +0200

    receptionist and calc-cost

--- lib/calc-cost/Booking.dart
+++ lib/calc-cost/Booking.dart
@@ -1,56 +1,39 @@
 import 'package:design_patterns_project/Abstract%20Boarding%20Option/AbstractBoardingOption.dart';
-<<<<<<< HEAD
 import 'package:design_patterns_project/abstract_room.dart';
-=======
->>>>>>> 445f2e0 (calculate cost)
 
 class Booking {
-   int _id;
-   int _DurationOfStay;
-<<<<<<< HEAD
-  AbstractRoom _room ;
-=======
-   AbstarctRoom _room ;
->>>>>>> 445f2e0 (calculate cost)
-   AbstractBoardingOption _boardingOption;
+  int _id;
+  int _DurationOfStay;
+  AbstractRoom _room;
+  AbstractBoardingOption _boardingOption;
 
+  Booking(this._id, this._DurationOfStay, this._room, this._boardingOption);
 
-   Booking(this._id,this._DurationOfStay,this._room,this._boardingOption);
+  int get id => _id;
 
-  
-   int get id => _id;
-
-  
   set id(int newId) {
     _id = newId;
   }
 
- 
   int get DurationOfStay => _DurationOfStay;
 
-  
   set DurationOfStay(int newDuration) {
     _DurationOfStay = newDuration;
   }
 
- 
   AbstractRoom get room => _room;
 
- 
   set room(AbstractRoom newRoom) {
     _room = newRoom;
   }
 
-  
   AbstractBoardingOption get boardingOption => _boardingOption;
 
-  
   set boardingOption(AbstractBoardingOption newBoardingOption) {
     _boardingOption = newBoardingOption;
   }
- void displayDetails(){
-      print('$_DurationOfStay $_boardingOption $_room $_id');
- }
-  
 
+  void displayDetails() {
+    print('$_DurationOfStay $_boardingOption $_room $_id');
+  }
 }
--- lib/calc-cost/HotelCheckoutService.dart
+++ lib/calc-cost/HotelCheckoutService.dart
@@ -1,34 +1,26 @@
-<<<<<<< HEAD
 import 'package:design_patterns_project/abstract_room.dart';
-=======
->>>>>>> 445f2e0 (calculate cost)
 import 'package:design_patterns_project/calc-cost/CostCalculationStrategy.dart';
 import 'package:flutter/material.dart';
 
 class HotelCheckoutService {
-AbstractRoom _Room ;
-CostCalculationStrategy _boardingStrategy;
+  AbstractRoom _Room;
+  CostCalculationStrategy _boardingStrategy;
 
-HotelCheckoutService(this._Room,this._boardingStrategy);
- 
+  HotelCheckoutService(this._Room, this._boardingStrategy);
 
+  AbstractRoom get Room => _Room;
 
-   AbstractRoom get Room => _Room;
+  set Room(AbstractRoom newRoom) {
+    _Room = newRoom;
+  }
 
-   set Room(AbstractRoom newRoom){
-    _Room=newRoom;
-   }
+  CostCalculationStrategy get boardingStrategy => _boardingStrategy;
 
-   CostCalculationStrategy get boardingStrategy=>_boardingStrategy;
+  set boardingStrategy(CostCalculationStrategy newStrategy) {
+    _boardingStrategy = newStrategy;
+  }
 
-   set boardingStrategy(CostCalculationStrategy newStrategy){
-    _boardingStrategy=newStrategy;
-
-   }
-
-   calculateTotalCost(int nights){
-      return _boardingStrategy.calculateCost(nights);
-   }
-
-
+  calculateTotalCost(int nights) {
+    return _boardingStrategy.calculateCost(nights);
+  }
 }
