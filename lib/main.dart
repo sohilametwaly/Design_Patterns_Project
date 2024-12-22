2bcb4a82533c0576eed8bc9d6f5f4c95370b87d0
engy medhat <mengy289@gmail.com>
2024-12-22 21:53:05 +0200

    some edits

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,6 @@ import 'package:design_patterns_project/resident_list/ResidentListScreen.dart';
 import 'package:design_patterns_project/resident_list/addResident.dart';
 
 import 'package:design_patterns_project/Database.dart';
-import 'package:design_patterns_project/signup/signuppage.dart';
-
-import 'package:flutter/material.dart';
-// import 'package:design_patterns_project/Manager/Manager.dart';
-// import 'package:design_patterns_project/Manager/WorkerManager.dart';
-// import 'package:design_patterns_project/Manager/IncomeTracker.dart';
-// import 'package:design_patterns_project/Manager/ResidentViewer.dart';
-// import 'package:design_patterns_project/Manager/RoomMonitor.dart';
-// import 'package:design_patterns_project/workerList.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'Receptionist.dart';
-import 'roomAssigner.dart';
-import 'ResidentManagement.dart';
-import 'residentList.dart';
-
-import 'package:design_patterns_project/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'login/loginpage.dart';
@@ -33,13 +16,11 @@ import 'Manager/WorkerManager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
