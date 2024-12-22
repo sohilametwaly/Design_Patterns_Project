2cae89b3c2961abe109474785b4e6eb4415cd3b8
engy medhat <mengy289@gmail.com>
2024-12-22 20:18:19 +0200

    some edits

--- lib/room_factory.dart
+++ lib/room_factory.dart
@@ -2,14 +2,22 @@ import 'abstract_room.dart';
 import 'single_room.dart';
 import 'double_room.dart';
 import 'triple_room.dart';
-
+import 'Database.dart';
 
 class RoomFactory {
   static AbstractRoom createRoom(String roomType, int roomNumber) {
+  
+  static void createRoom(String roomType, int roomNumber) {
+     Database database = Database.getInstance();
+     Map<String,dynamic> room ={'Room_type':roomType,'Room_number':roomNumber,''}
+    //  AbstractRoom? currentRoom =
+
+    //   String userId = currentUser!.uid;
+
     if (roomType == "Single") {
-      return SingleRoom(roomNumber);
+       database.writeData('Rooms/', data)
     }else if(roomType == "Double"){
-      return DoubleRoom(roomNumber);
+       DoubleRoom(roomNumber);
     }else if(roomType == "Triple"){
       return TripleRoom(roomNumber);
     }else{
