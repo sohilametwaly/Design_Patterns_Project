c74ce9ac630266c5b709fc710a624f35b5cf6ffd
Basant <basantabdelnaser438@gmail.com>
2024-12-23 13:51:44 +0200

    room list after updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 
+import 'package:design_patterns_project/Manager/IncomeTracker.dart';
+import 'package:design_patterns_project/Manager/Manager.dart';
+import 'package:design_patterns_project/Manager/ResidentViewer.dart';
+import 'package:design_patterns_project/Manager/RoomMonitor.dart';
+import 'package:design_patterns_project/Manager/RoomMonitoring.dart';
 import 'package:design_patterns_project/Room_list_screen.dart';
 import 'package:design_patterns_project/resident_list/ResidentListScreen.dart';
 import 'package:design_patterns_project/resident_list/addResident.dart';
@@ -6,6 +11,8 @@ import 'package:design_patterns_project/resident_list/addResident.dart';
 import 'package:design_patterns_project/Database.dart';
 
 import 'package:flutter/material.dart';
+
+import 'Manager/WorkerManager.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,9 +28,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+     WorkerManager workerManager= WorkerManager();
+     Incometracker incometracker= Incometracker();
+     Roommonitor roommonitor=Roommonitor();
+     Residentviewer residentViewer = Residentviewer();
+    Manager m = Manager(workerManager: workerManager, incomeTracker: incometracker, roomMonitor:roommonitor, residentViewer: residentViewer);
+    // final Roommonitoring roomMonitor;
+    // final Residentviewer residentViewer;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RoomListScreen(),
+      home: RoomListScreen(manager: m),
     );
   }
 }
