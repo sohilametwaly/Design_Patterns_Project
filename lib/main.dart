f9322d404d249875a31f4e1e066c17afd5ffbf09
engy medhat <mengy289@gmail.com>
2024-12-23 17:20:58 +0200

    adding functions in income tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 <<<<<<< HEAD
 =======
 
+import 'package:design_patterns_project/Manager/IncomeTracker.dart';
+import 'package:design_patterns_project/Manager/Manager.dart';
+import 'package:design_patterns_project/Manager/ResidentViewer.dart';
+import 'package:design_patterns_project/Manager/RoomMonitor.dart';
+import 'package:design_patterns_project/Manager/RoomMonitoring.dart';
 import 'package:design_patterns_project/Room_list_screen.dart';
 import 'package:design_patterns_project/resident_list/ResidentListScreen.dart';
 import 'package:design_patterns_project/resident_list/addResident.dart';
@@ -21,7 +26,13 @@ import 'roomAssigner.dart';
 import 'ResidentManagement.dart';
 import 'residentList.dart';
 
+<<<<<<< HEAD
 void main() async {
+=======
+import 'Manager/WorkerManager.dart';
+
+void main() {
+>>>>>>> c74ce9a (room list after updates)
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MainApp());
@@ -30,6 +41,7 @@ void main() async {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     // WorkerManager workerManager = WorkerManager();
     // Incometracker incometracker = Incometracker();
     // Roommonitor roommonitor = Roommonitor();
@@ -53,6 +65,18 @@ class MainApp extends StatelessWidget {
 =======
       home: RoomListScreen(),
 >>>>>>> e6054a5 (room-list-done)
+=======
+     WorkerManager workerManager= WorkerManager();
+     Incometracker incometracker= Incometracker();
+     Roommonitor roommonitor=Roommonitor();
+     Residentviewer residentViewer = Residentviewer();
+    Manager m = Manager(workerManager: workerManager, incomeTracker: incometracker, roomMonitor:roommonitor, residentViewer: residentViewer);
+    // final Roommonitoring roomMonitor;
+    // final Residentviewer residentViewer;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RoomListScreen(manager: m),
+>>>>>>> c74ce9a (room list after updates)
     );
   }
 }
