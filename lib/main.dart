2a8c5b03122c9356e041df1706f874483dd14c59
sohilametwaly <sohailarafat934@yahoo.com>
2024-12-24 21:41:21 +0200

    "Merging"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
-import 'package:design_patterns_project/Classes/Manager/IncomeTracker.dart';
-import 'package:design_patterns_project/Classes/Manager/Manager.dart';
-import 'package:design_patterns_project/Classes/Manager/ResidentViewer.dart';
-import 'package:design_patterns_project/Classes/Manager/RoomMonitor.dart';
-
-import 'package:design_patterns_project/Classes/Receptionist.dart';
-import 'package:design_patterns_project/Classes/ResidentManagement.dart';
-
-import 'package:design_patterns_project/Classes/roomAssigner.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/login/loginpage.dart';
-
-import 'Classes/Manager/WorkerManager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,20 +13,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ResidentManagement residentManagement = ResidentManagement();
-    RoomAssigner roomAssigner = RoomAssigner();
-    Receptionist receptionist = Receptionist(residentManagement, roomAssigner);
-    WorkerManager workerManager = WorkerManager();
-    Incometracker incometracker = Incometracker();
-    Roommonitor roommonitor = Roommonitor();
-    Residentviewer residentViewer = Residentviewer();
-
-    Manager m = Manager(
-        workerManager: workerManager,
-        incomeTracker: incometracker,
-        roomMonitor: roommonitor,
-        residentViewer: residentViewer);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //title: 'Firebase Sign Up',
